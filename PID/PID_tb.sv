7f680240093232e1987b3291c7167275b8cc5f7c
07yhxiaxy <101749782+07yhxiaxy@users.noreply.github.com>
2022-04-06 12:46:35 -0500

    added generate for FAST_SIM signal

--- PID/PID_tb.sv
+++ PID/PID_tb.sv
@@ -1,5 +1,9 @@
 module PID_tb();
 
+parameter FAST_SIM;
+logic [19:0] decimator;
+logic decimator_full;
+
 logic clk, rst_n, not_pedaling;
 logic [11:0] drv_mag;
 logic [12:0] error;
@@ -7,6 +11,12 @@ logic test_over;
 
 plant_PID plant(.clk(clk), .rst_n(rst_n), .drv_mag(drv_mag), .error(error), .not_pedaling(not_pedaling), .test_over(test_over));
 PID_tb iDUT(.clk(clk), .rst_n(rst_n), .drv_mag(drv_mag), .error(error), .not_pedaling(not_pedaling));
+
+generate if (FAST_SIM)
+	assign decimator_full = &decimator[14:0];
+else
+	assign decimator_full = &decimator;
+endgenerate
 
 always
 	#5 clk = ~clk;
