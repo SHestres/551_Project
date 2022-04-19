9773a0f62fb56144f41f8732ee7b738b40f8c620
mattwyz <102988056+mattwyz@users.noreply.github.com>
2022-04-19 18:00:28 -0500

    Add files via upload

--- A2D/A2D_intf.sv
+++ A2D/A2D_intf.sv
@@ -10,31 +10,38 @@ module A2D_intf(
   logic [15:0] cmd;
   logic en_ba, en_c, en_br, en_t, snd, done, cnv_cmplt;
   logic [15:0] resp;
+  
+  // enum for A2D state machine
   typedef enum logic [1:0] {IDLE, FIRST, WAIT, SEC} state_t;
   state_t state, nxt_state;
   
+  // SPI used to transfer data
   SPI_mnrch SPI(.clk(clk), .rst_n(rst_n), .snd(snd), .MISO(MISO), .cmd(cmd),
 			.done(done), .SS_n(SS_n), .SCLK(SCLK), .MOSI(MOSI), .resp(resp));
   
-  always_ff @(posedge clk, negedge rst_n) begin				// 14 bit counter, when full start transfer
+  // 14 bit counter, when full, start transfer
+  always_ff @(posedge clk, negedge rst_n) begin				
     if (!rst_n)
 	  cnt_14 <= 14'h00000;
 	else
 	  cnt_14 <= cnt_14 + 1'b1;
   end  
   
-  always_ff @(posedge clk, negedge rst_n) begin					// cycle through channel reads when previous read is done
+  // cycle through channel reads when previous read is done
+  always_ff @(posedge clk, negedge rst_n) begin					
     if (!rst_n)
 	  cnt_2 <= 2'b00;
 	else if (cnv_cmplt)
 	  cnt_2 <= cnt_2 + 1'b1;
   end
   
-  assign cmd = 	(cnt_2 == 2'b00) ? {2'b00, 3'b000, 11'h000} :		//  assign cmd based on cnt_2
+  // assign cmd based on channel selected by cnt_2
+  assign cmd = 	(cnt_2 == 2'b00) ? {2'b00, 3'b000, 11'h000} :		
 				(cnt_2 == 2'b01) ? {2'b00, 3'b001, 11'h000} :		
 				(cnt_2 == 2'b10) ? {2'b00, 3'b011, 11'h000} :
 				{2'b00, 3'b100, 11'h000};
   
+  // A2D state flop
   always @(posedge clk, negedge rst_n) begin
     if (!rst_n)
 	  state <= IDLE;
@@ -42,6 +49,7 @@ module A2D_intf(
 	  state <= nxt_state;
   end
   
+  // A2D next state and output logic
   always_comb begin
     nxt_state = state;
 	snd = 1'b0;
@@ -71,6 +79,7 @@ module A2D_intf(
   assign en_t = (cnt_2 == 2'b11 & cnv_cmplt) ? 1'b1 : 1'b0;				//
   
   
+  // update reg values when enabled
   always_ff@(posedge clk, negedge rst_n) begin
 	if (!rst_n) begin
 	  batt <= 12'h000;
--- A2D/A2D_intf_tb.sv
+++ A2D/A2D_intf_tb.sv
@@ -11,6 +11,7 @@ module A2D_intf_tb();
   A2D_intf A2D(.clk(clk), .rst_n(rst_n), .batt(batt), .curr(curr), 
   .brake(brake), .torque(torque), .SS_n(SS_n), .SCLK(SCLK), .MOSI(MOSI), .MISO(MISO));
   
+  
   initial begin
     rst_n = 0;
 	clk = 0;
@@ -18,12 +19,57 @@ module A2D_intf_tb();
 	@(negedge clk);
 	rst_n = 1;
 	repeat (100000) @(posedge clk);
+	
+	// all reg values should be updated with 12'hFFF
+	if (batt !== 12'hFFF) begin
+	  $display("Test 1: failed, batt did not match expected");
+	  $stop;
+	end 
+	
+	if (curr !== 12'hFFF) begin
+	  $display("Test 2: failed, curr did not match expected");
+	  $stop;
+	end 
+	
+	if (brake !== 12'hFFF) begin
+	  $display("Test 3: failed, brake did not match expected");
+	  $stop;
+	end 
+	
+	if (torque !== 12'hFFF) begin
+	  $display("Test 4: failed, torque did not match expected");
+	  $stop;
+	end 
+	
 	@(negedge clk);
 	rst_n = 0;
 	A2D_data = 15'h0001;
 	@(negedge clk);
 	rst_n = 1;
 	repeat (100000) @(posedge clk);
+	
+	// all reg values should be updated with 12'h001
+	if (batt !== 12'h001) begin
+	  $display("Test 5: failed, batt did not match expected");
+	  $stop;
+	end 
+	
+	if (curr !== 12'h001) begin
+	  $display("Test 6: failed, curr did not match expected");
+	  $stop;
+	end 
+	
+	if (brake !== 12'h001) begin
+	  $display("Test 7: failed, brake did not match expected");
+	  $stop;
+	end 
+	
+	if (torque !== 12'h001) begin
+	  $display("Test 8: failed, torque did not match expected");
+	  $stop;
+	end 
+	
+	$display("All tests passed");
 	$stop;
   end
   
