7e7d164c9292d142ae0d7cde2dd939efb6064a60
mattwyz <mwzietz12@gmail.com>
2022-05-05 19:36:57 -0500

    fix desired_drive

--- eBike/desiredDrive.sv
+++ eBike/desiredDrive.sv
@@ -15,7 +15,7 @@ logic signed [10:0]incline_factor;
 logic [8:0]incline_lim;
 logic [12:0]torque_off;
 logic [11:0]torque_pos;
-logic [29:0]assist_prod;
+logic [29:0]assist_prod, assist_prod_1;
 logic [5:0]cadence_factor;
 
 incline_sat iInSat(.incline(incline), .incline_sat(inclineSat));
@@ -50,13 +50,15 @@ end: pipeline_flops
 logic [14:0]mult1;
 logic [14:0]mult2;
 always_ff @(posedge clk) begin: second_pipeline
-mult1 <= torque_pos * scale;
-mult2 <= incline_lim * cadence_factor;
-assist_prod <= mult1 * mult2;
+	mult1 <= torque_pos * scale;
+	mult2 <= incline_lim * cadence_factor;
+	assist_prod <= mult1 * mult2;
 end: second_pipeline
 
 //assign assist_prod = not_pedaling ? 0 : torque_pos * incline_lim * cadence_factor * scale;
 
-assign target_curr = |assist_prod[29:27] ? 12'hFFF : assist_prod[26:15];
+assign assist_prod_1 = not_pedaling ? 0 : assist_prod;
+
+assign target_curr = |assist_prod_1[29:27] ? 12'hFFF : assist_prod_1[26:15];
 
 endmodule
