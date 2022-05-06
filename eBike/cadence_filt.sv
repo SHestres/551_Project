bffca5a691760f9b0977067492cfb9690fad7848
07yhxiaxy <101749782+07yhxiaxy@users.noreply.github.com>
2022-05-06 15:02:27 -0500

    Modified to meet format
    separated two ff according to guideline that ff with different functions should be written separately

--- eBike/cadence_filt.sv
+++ eBike/cadence_filt.sv
@@ -1,47 +1,51 @@
 module cadence_filt#(parameter FAST_SIM = 1)(clk, rst_n, cadence, cadence_filt, cadence_rise);
+input clk, rst_n, cadence;
+output cadence_filt, cadence_rise;
 
-input logic clk, rst_n, cadence;
-output logic cadence_filt, cadence_rise;
-
-//parameter FAST_SIM;
-
-logic q1, q2, q3; //Metastability flop outputs and delay input flop
-logic chngd_n;
-logic [15:0] count; //Internal signal to count stability
+// cadence_filt outputs cadence cadence is held stable for 1 ms.
+// cadence_rise detects whether the cadence has a rising edge.
+logic cadence_ff1, cadence_ff2, cadence_ff3;
+logic cadence_rise, cadence_filt;
+logic stable;
 logic [15:0] stbl_cnt;
 
-generate
-always @(posedge(clk), negedge(rst_n)) begin : flops
-if(!rst_n) begin
-q1 <= 1'b0;
-q2 <= 1'b0;
-cadence_filt <= 01'b0;
+always_ff @(posedge clk, negedge rst_n) begin
+	if (~rst_n) begin
+		cadence_ff1 <= 0;
+		cadence_ff2 <= 0;
+		cadence_ff3 <= 0;
+	end
+	else begin
+		cadence_ff1 <= cadence;
+		cadence_ff2 <= cadence_ff1;
+		cadence_ff3 <= cadence_ff2;
+	end
 end
-else begin
-	q1 <= cadence;
-	q2 <= q1;
-	q3 <= q2;
-	
-	stbl_cnt <= count;
+
+// Separate the ff because they're different functions
+// This ff tells when the candence stabilizes
+generate
+always_ff @(posedge clk, negedge rst_n) begin
+	if ((cadence_ff2 != cadence_ff3) || (~rst_n))
+		stbl_cnt <= 0;
+	else
+		stbl_cnt <= stbl_cnt + 1;
+
 	if(!FAST_SIM) begin
-		if(&stbl_cnt)
-			cadence_filt <= q3;
+		if (stable) 
+			cadence_filt <= cadence_ff3;
 	end
 	else begin
 		if(&stbl_cnt[8:0])
-			cadence_filt <= q3;
+			cadence_filt <= cadence_ff3;
 	end
 end
-end : flops
 endgenerate
 
+
 always_comb begin
-cadence_rise = q2 & !q3;
-chngd_n = !(q2^q3);
-
-count = {16{chngd_n}} & (stbl_cnt + 1);
+	stable = &stbl_cnt;
+	cadence_rise = cadence_ff2 & ~cadence_ff3;
 end
-
-
 
 endmodule
