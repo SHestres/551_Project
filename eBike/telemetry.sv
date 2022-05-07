50efa5a9591427ebcd81c5da8c7f1b97d81c625c
07yhxiaxy <101749782+07yhxiaxy@users.noreply.github.com>
2022-05-07 18:33:39 -0500

    Gave meaningful name to telemetry

--- eBike/telemetry.sv
+++ eBike/telemetry.sv
@@ -1,171 +1,12 @@
-// module telemetry(clk, rst_n, batt_v, avg_curr, avg_torque, TX);
-// input clk, rst_n;
-// input [11:0] batt_v, avg_curr, avg_torque;
-// output logic TX;
+module telemetry (
+	input clk, rst_n,
+	input [11:0] batt_v, avg_curr, avg_torque,
+	output logic TX
+);
 
-// typedef enum logic [3:0] {START, FIRST_TRMT, FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH, EIGHTH, HOLD} state_t;
+typedef enum logic [3:0] {NADA, TX_START, DELIM_1, DELIM_2, BATT_HIGH, BATT_LOW, CURR_HIGH, CURR_LOW, TRQ_HIGH, TRQ_LOW, HOLD_HIGH} state_t;
 
-
-// logic [7:0] tx_data;
-// logic trmt, tx_done;
-
-// logic [20:0] cntr;
-// logic cnt_done, trmt_set;
-// state_t state, next_state;
-
-// UART_tx uart(.clk(clk), .rst_n(rst_n), .tx_data(tx_data), .trmt(trmt), .tx_done(tx_done), .TX(TX));
-
-// always_ff @(posedge clk, negedge rst_n) begin
-// 	if (!rst_n)
-// 		cntr <= 0;
-// 	else if (cnt_done)
-// 		cntr <= 0;
-//              else	
-// 		cntr <= cntr + 1;
-// end
-// assign cnt_done = cntr[20];
-
-// always_ff @(posedge clk, negedge rst_n) begin
-// 	if (!rst_n)
-// 		state <= START;
-// 	else 
-// 		state <= next_state;	
-// end
-
-// always_ff @(posedge clk, negedge rst_n) begin
-// 	if (!rst_n)
-// 		 trmt <= 0;
-// 	else if (trmt_set) 
-// 	        trmt  <= 1;	
-// 	     else
-// 			trmt <= 0;
-// end
-
-
-// always_comb begin
-// 	case (state)
-// 		START: begin
-// 			if (rst_n)
-// 				next_state = FIRST_TRMT;
-// 			else
-// 				next_state = next_state;
-// 			end
-
-// 		FIRST_TRMT: begin
-// 			next_state = FIRST;
-// 			trmt_set = 1;
-// 			tx_data = 8'hAA;
-// 		end
-
-// 		FIRST: begin
-// 			if (tx_done) begin
-// 				next_state = SECOND;
-// 				trmt_set = 1;
-// 			end
-// 			else  begin
-// 				next_state = next_state;
-// 				trmt_set = 0;
-// 			end
-// 		       	tx_data = 8'hAA;
-// 	       	end
-
-
-// 		SECOND: begin
-// 			if (tx_done) begin
-// 				next_state = THIRD;
-// 				trmt_set = 1;
-// 			end
-// 			else begin
-// 				next_state = next_state;
-// 				trmt_set = 0;
-// 			end
-// 			tx_data = 8'h55; 
-// 			end
-
-// 		THIRD: begin
-// 			if (tx_done) begin
-// 				next_state = FOURTH;
-// 				trmt_set = 1;
-// 			end
-// 			else begin
-// 				next_state = next_state;
-// 				trmt_set = 0;
-// 			end
-// 			tx_data = {4'h0, batt_v[11:8]};
-// 			end
-
-// 		FOURTH: begin
-// 			if (tx_done) begin
-// 				next_state = FIFTH;
-// 				trmt_set = 1;
-// 			end
-// 			else begin
-// 				next_state = next_state;
-// 				trmt_set = 0;
-// 			end
-// 			tx_data = batt_v[7:0];
-// 			end
-// 		FIFTH: begin
-// 			if (tx_done) begin
-// 				next_state = SIXTH;
-// 				trmt_set = 1;
-// 			end
-// 			else begin
-// 				next_state = next_state;
-// 				trmt_set = 0;
-// 			end
-// 			tx_data = {4'h0, avg_curr[11:8]};
-// 			end
-// 		SIXTH: begin
-// 			if (tx_done) begin
-// 				next_state = SEVENTH;
-// 				trmt_set = 1;
-// 			end
-// 			else begin
-// 				next_state = next_state;
-// 				trmt_set = 0;
-// 			end
-// 			tx_data = avg_curr[7:0];
-// 			end
-// 		SEVENTH: begin
-// 			if (tx_done) begin
-// 				next_state = EIGHTH;
-// 				trmt_set = 1;
-// 			end
-// 			else begin
-// 				next_state = next_state;
-// 				trmt_set = 0;
-// 			end
-// 			tx_data = {4'h0, avg_torque[11:8]};
-// 			end
-// 		EIGHTH: begin
-// 			trmt_set = 0;
-// 			if (tx_done)
-// 				next_state = HOLD;
-// 			else
-// 				next_state = next_state;
-// 				tx_data = avg_torque[7:0];
-// 			end
-// 		HOLD: begin
-// 			trmt_set = 0;
-// 			if (cnt_done)
-// 				next_state = FIRST;
-// 			else
-// 				next_state = next_state;
-// 			end
-
-// 			endcase
-//      end
-// endmodule
-
-module telemetry(clk, rst_n, batt_v, avg_curr, avg_torque, TX);
-input clk, rst_n;
-input [11:0] batt_v, avg_curr, avg_torque;
-output logic TX;
-
-typedef enum logic [3:0] {START, FIRST_TRMT, FIRST, SECOND, THIRD, FOURTH, FIFTH, SIXTH, SEVENTH, EIGHTH, HOLD} state_t;
-
-
+// Decare transmission state logics
 logic [7:0] tx_data;
 logic trmt, tx_done;
 
@@ -173,69 +14,48 @@ logic [20:0] cntr;
 logic cnt_done, trmt_set;
 state_t state, next_state;
 
-UART_tx xmitter(.clk(clk), .rst_n(rst_n), .tx_data(tx_data), .trmt(trmt), .tx_done(tx_done), .TX(TX));
+// instantiate the UART module
+UART_tx uart(.clk(clk), .rst_n(rst_n), .tx_data(tx_data), .trmt(trmt), .tx_done(tx_done), .TX(TX));
 
-// Counter that helps sending info 47 times / sec
+// Declare the FSM
 always_ff @(posedge clk, negedge rst_n) begin
 	if (!rst_n)
-		cntr <= 0;
-	else if (cnt_done)
-		cntr <= 0;
-		else	
-		cntr <= cntr + 1;
-end
-assign cnt_done = cntr[20];
-
-// FSM state ff
-always_ff @(posedge clk, negedge rst_n) begin
-	if (!rst_n)
-		state <= START;
+		state <= NADA;
 	else 
 		state <= next_state;	
 end
 
-// ff that controls UART
-always_ff @(posedge clk, negedge rst_n) begin
-	if (!rst_n)
-		trmt <= 0;
-	else if (trmt_set) 
-		trmt  <= 1;	
-		else
-		trmt <= 0;
-end
-
-
 always_comb begin
 	case (state)
-		START: begin
+	// Kickoff the transmission
+		NADA: begin
 			if (rst_n)
-				next_state = FIRST_TRMT;
+				next_state = TX_START;
 			else
 				next_state = next_state;
-		       end
-
-		FIRST_TRMT: begin
-			next_state = FIRST;
+			end
+		TX_START: begin
+			next_state = DELIM_1;
 			trmt_set = 1;
 			tx_data = 8'hAA;
 		end
-
-		FIRST: begin
+	// Send DELIMITER
+		DELIM_1: begin
 			if (tx_done) begin
-				next_state = SECOND;
+				next_state = DELIM_2;
 				trmt_set = 1;
-		        end
-			else begin
+			end
+			else  begin
 				next_state = next_state;
 				trmt_set = 0;
 			end
-		       tx_data = 8'hAA;
-	       end
+		       	tx_data = 8'hAA;
+	       	end
 
 
-		SECOND: begin
+		DELIM_2: begin
 			if (tx_done) begin
-				next_state = THIRD;
+				next_state = BATT_HIGH;
 				trmt_set = 1;
 			end
 			else begin
@@ -243,11 +63,11 @@ always_comb begin
 				trmt_set = 0;
 			end
 			tx_data = 8'h55; 
-		       end
-
-		THIRD: begin
+			end
+	// SEND BATT
+		BATT_HIGH: begin
 			if (tx_done) begin
-				next_state = FOURTH;
+				next_state = BATT_LOW;
 				trmt_set = 1;
 			end
 			else begin
@@ -255,11 +75,11 @@ always_comb begin
 				trmt_set = 0;
 			end
 			tx_data = {4'h0, batt_v[11:8]};
-		       end
+			end
 
-		FOURTH: begin
+		BATT_LOW: begin
 			if (tx_done) begin
-				next_state = FIFTH;
+				next_state = CURR_HIGH;
 				trmt_set = 1;
 			end
 			else begin
@@ -267,10 +87,11 @@ always_comb begin
 				trmt_set = 0;
 			end
 			tx_data = batt_v[7:0];
-		       end
-		FIFTH: begin
+			end
+	// SEND AVG CURR
+		CURR_HIGH: begin
 			if (tx_done) begin
-				next_state = SIXTH;
+				next_state = CURR_LOW;
 				trmt_set = 1;
 			end
 			else begin
@@ -278,10 +99,10 @@ always_comb begin
 				trmt_set = 0;
 			end
 			tx_data = {4'h0, avg_curr[11:8]};
-		       end
-		SIXTH: begin
+			end
+		CURR_LOW: begin
 			if (tx_done) begin
-				next_state = SEVENTH;
+				next_state = TRQ_HIGH;
 				trmt_set = 1;
 			end
 			else begin
@@ -289,10 +110,11 @@ always_comb begin
 				trmt_set = 0;
 			end
 			tx_data = avg_curr[7:0];
-		       end
-		SEVENTH: begin
+			end
+	// SEND AVG TRQ
+		TRQ_HIGH: begin
 			if (tx_done) begin
-				next_state = EIGHTH;
+				next_state = TRQ_LOW;
 				trmt_set = 1;
 			end
 			else begin
@@ -300,23 +122,46 @@ always_comb begin
 				trmt_set = 0;
 			end
 			tx_data = {4'h0, avg_torque[11:8]};
-		       end
-		EIGHTH: begin
+			end
+		TRQ_LOW: begin
 			trmt_set = 0;
 			if (tx_done)
-				next_state = HOLD;
+				next_state = HOLD_HIGH;
 			else
 				next_state = next_state;
-			tx_data = avg_torque[7:0];
-		       end
-		HOLD: begin
+				tx_data = avg_torque[7:0];
+			end
+		// FINISH TRANSMISSION
+		HOLD_HIGH: begin
 			trmt_set = 0;
 			if (cnt_done)
-				next_state = FIRST;
+				next_state = DELIM_1;
 			else
 				next_state = next_state;
-		       end
+			end
 
-             endcase
+			endcase
      end
+
+// Declare the transmission states
+always_ff @(posedge clk, negedge rst_n) begin
+	if (!rst_n)
+		trmt <= 0;
+	else if (trmt_set) 
+		trmt  <= 1;	
+	else
+		trmt <= 0;
+end
+
+// Declare a counter register
+always_ff @(posedge clk, negedge rst_n) begin
+	if (!rst_n)
+		cntr <= 0;
+	else if (cnt_done)
+		cntr <= 0;
+	else	
+		cntr <= cntr + 1;
+end
+assign cnt_done = cntr[20];
+
 endmodule
